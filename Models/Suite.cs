7ade4ec567c9d8c4c0852b21833dab5eff57662e
Juliana Camilo <104179960+julianacamilo@users.noreply.github.com>
2022-09-06 16:46:02 -0300

    Add files via upload

---
+++ Models/Suite.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospedagemHotel.Models
+{
+    public class Suite
+    {
+        
+    }
+}
