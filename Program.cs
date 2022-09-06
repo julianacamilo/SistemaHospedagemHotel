652208f0437b6518e4096c5eccd2f2c584506af1
Juliana Camilo <104179960+julianacamilo@users.noreply.github.com>
2022-09-06 16:46:56 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using HospedagemHotel.Models;
+using HospedagemHotel.Models.DesafioProjetoHospedagem.Models;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+
+try
+{
+   
+
+    Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+
+   
+    Reserva reserva = new Reserva(diasReservados: 10);
+    reserva.CadastrarSuite(suite);
+    reserva.CadastrarHospedes(hospedes);
+
+   
+    Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+    Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+    Console.ReadKey();
+
